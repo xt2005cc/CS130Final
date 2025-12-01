1c5ecdd71604cefcda41a2902ae9e1d59beb29d8
xt2005cc <christian2005cruz@hotmail.com>
2025-12-01 11:13:37 -0800

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -30,3 +30,47 @@ struct Student{
         return age;
     }
 };
+
+istream& operator>>(istream &in, Student &s){
+    if(!(in>>s.name>>s.age)){
+        return in;
+    }
+    s.scores.sum = 0;
+    for (int i = 0; i <3; i++){
+        in>>s.scores.arr[i];
+        s.scores.sum = s.scores.arr[i];
+    }
+    s.scores.avg = s.scores.sum / 3;
+    return in;
+}
+
+ostream& operator<<(ostream &out, const Student &s){
+    out << s.name << " (" << s.age <<") Scores = ";
+    for (int i = 0; i < 3; i++){
+        out << s.scores.arr[i] << " ";
+    }
+    out << "Avg = " << s.scores.avg
+    return out;
+}
+
+int searchStudent(const vector<Student> &students, const string &key){
+    for (int i = 0; i < students.size(); i++){
+        if(students[i].getName() == key){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    ifstream ifs("students.data");
+    vector<Student> stu;
+    Student temp;
+
+    if(!ifs){
+        cout << "Error";
+        return 1;
+    }
+
+    cout << "Reading students"
+}
